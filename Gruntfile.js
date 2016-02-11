263fb8e787403cafb4cf6cc79608bcbd3c9e3943
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 12:38:54 -0800

    another test

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,6 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-      options: {
-        separator: ';',
-      },
       dist: {
         src: ['client/**/*.js'],
         dest: 'public/dist/main.js',
